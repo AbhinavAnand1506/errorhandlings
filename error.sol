d0132732a2e116be78a773eb3f5d4687810efa5e
AbhinavAnand1506 <110054119+AbhinavAnand1506@users.noreply.github.com>
2024-03-15 12:49:52 +0530

    Update error.sol

--- error.sol
+++ error.sol
@@ -1,46 +1,58 @@
-//There are only three statement to solve error require(), assert() and revert().
-//require() is used to validate certain condition before execution of a function. 
-//It takes two parameter as a input, the first is to check the condition and second is to send
-// the messsage if conditiion  fails. It works only with a single condition check parameter.
-//The assert() function is a convinience that checks for conditions, if a condition fails, then 
-//the function execution is terminated with a error msg.
-//revert() can be used to flag an error and revert the current call, it can also provide a 
-// msg containing details about the error and the msg will be passed back to the caller.
+//SPDX-License-Identifier: MIT
+//Error handling in Solidity mainly involves the require, assert, and revert
+//statements to check conditions and revert transactions.
+//Lets discuss them:
+// Require is used for:
+//    -inputs
+//    -conditions before execution
+//    - return values from calls to other functions
 
+// Assert is used to test for internal errors, and to check invariants.
+// Here we assert that num is always equal to 0
+// since it is impossible to update the value of num
+// This function deliberately causes an error by reverting the transaction
+// Revert is useful when the condition to check is complex.
+// This code does the exact same thing as the example above
 
+//This contract represents a simple voting system where users can vote yes or no on a proposal. Each user can only vote once.
+// The contract keeps track of the number of yes and no votes, 
+//and users can get the result of the vote through the getResult function.
 
-pragma solidity 0.8.22;
+pragma solidity ^0.8.0;
 
-contract Errorhandling {
-    function testRequire(uint _i) public pure {
-        // Require should be used to validate conditions such as:
-        // - inputs
-        // - conditions before execution
-        // - return values from calls to other functions
-        require(_i > 25, "Input must be greater than 25");
-        //if this condition will satisfy then rest of the function will work
-        // otherwise it should back the message.
+contract VotingSystem{
+    mapping(address => bool) public hasVoted;
+    uint public yesVotes;
+    uint public noVotes;
+    event Voted(address indexed voter, bool vote);
+    
+    //here we pass bool type which is either true or false.
+    function vote(bool inSupport) external {
+        
+        require(!hasVoted[msg.sender], "You have already voted");
+        //we used hasvote variable, if hasvote is not equals to message out sender then it works 
+        //propoerly otherwise it sent back the error you have already voted in.
+
+        //if we have voted on one address, then we are not able to vote again and again
+        //on that address. 
+
+       // if else statement is used to count no of yes or no votes.
+        if(inSupport){
+            yesVotes++;
+        }else{
+            noVotes++;
+        }
+        hasVoted[msg.sender]= true;
+        emit Voted(msg.sender, inSupport);
     }
-
-    function testRevert(uint _i) public pure {
-        // Revert is useful when the condition to check is complex.
-        // This code does the exact same thing as the example above
-        if (_i <= 25) {
-            revert("Input must be greater than 25");
+    function getResult() external view returns(string memory) {
+        if(yesVotes > noVotes){
+            return "Proposal accepted";
+        }
+        else if(noVotes > yesVotes){
+            return "Proposal rejected";
+        }else{
+            return "Proposal tied";
         }
     }
-
-    uint public num;
-
-    function testAssert() public view {
-        // Assert should only be used to test for internal errors,
-        // and to check invariants.
-
-        // Here we assert that num is always equal to 0
-        // since it is impossible to update the value of num
-        assert(num == 0);
-    }
-
-    
 }
-
